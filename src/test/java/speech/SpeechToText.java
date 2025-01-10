f5512962eac80c2fff8a055760f1844c9d0b263a
Artem Oganesyan <artem.oganesyan@gmail.com>
2025-01-09 20:05:25 -0800

    dd test added

--- src/test/java/speech/SpeechToText.java
+++ src/test/java/speech/SpeechToText.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class SpeechToText {
     private static final String API_KEY = System.getenv("KEY");
@@ -38,5 +39,16 @@ public class SpeechToText {
 
         String transcript = response.path("jsonResponse.results.transcripts[0].transcript");
         assertThat(transcript, equalTo("Hello, this is a test for text to speech conversion."));
+
+        String result = response.path("jsonResponse.status");
+        assertThat(result, equalTo("COMPLETED"));
+        boolean validMessage = false;
+        if (!transcript.contains("next")) {
+            validMessage = true;
+        }
+        assertThat(validMessage, equalTo(true));
+
+        List<Integer> segments = response.path("jsonResponse.results.audio_segments[0].items");
+        assertThat(segments.size(), greaterThan(10));
     }
 }
